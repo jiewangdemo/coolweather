a9253e6d6ce45cf9bdb8f482cc1def622dd7e22f
Wang <1602510768@qq.com>
2019-10-17 10:35:31 +0800

    create database

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,10 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by ASUS on 2019/10/17.
+ */
+
+public class City extends DataSupport {
+}
---
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -0,0 +1,8 @@
+package com.example.coolweather.db;
+
+/**
+ * Created by ASUS on 2019/10/17.
+ */
+
+public class Province {
+}
